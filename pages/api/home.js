790eb2ff1030661c427ce1c4d793ba073ff5545b
muxinqi <hi@mxq.im>
2021-03-19 12:25:16 +0800

    feat: add null catch up

--- pages/api/home.js
+++ pages/api/home.js
@@ -25,7 +25,11 @@ export default async function handler(req, res) {
         }
       }
     })
-    res.status(200).json(data)
+    if (!data) {
+      res.status(404)
+    } else {
+      res.status(200).json(data)
+    }
   } else {
     res.setHeader('Allow', ['GET'])
     res.status(405).end(`Method ${method} Not Allowed`)
