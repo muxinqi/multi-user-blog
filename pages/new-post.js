8ebf2d74383d7781a7b6141005f62bdd2d8cfce6
muxinqi <hi@mxq.im>
2021-03-22 10:02:36 +0800

    feat: implement post submit

--- pages/new-post.js
+++ pages/new-post.js
@@ -1,63 +1,156 @@
-import Header from "components/Header";
-import {AutoComplete, Button, Card, Col, Grid, Input, Page, Row, Spacer, Text} from "@geist-ui/react";
-import * as Icon from '@geist-ui/react-icons'
-import Editor from "rich-markdown-editor";
-import ReactMde, {SuggestionsDropdownProps} from "react-mde";
-import * as Showdown from "showdown";
-import "react-mde/lib/styles/css/react-mde-all.css";
+import {
+  AutoComplete,
+  Button,
+  Card,
+  Col,
+  Grid,
+  Input,
+  Loading,
+  Page,
+  Row,
+  Spacer,
+  Text,
+  useToasts
+} from "@geist-ui/react";
+import * as Icon from "@geist-ui/react-icons";
 import * as React from "react";
+import ReactMde from "react-mde";
+import ReactMarkdown from "react-markdown";
+import "react-mde/lib/styles/css/react-mde.css";
+import "react-mde/lib/styles/css/react-mde-toolbar.css";
+import "react-mde/lib/styles/css/react-mde-editor.css";
+import { useRouter } from "next/router";
 
-function loadSuggestions(text) {
-  return new Promise((accept, reject) => {
-    setTimeout(() => {
-      const suggestions = [
-        {
-          preview: "Andre",
-          value: "@andre"
-        },
-        {
-          preview: "Angela",
-          value: "@angela"
-        },
-        {
-          preview: "David",
-          value: "@david"
-        },
-        {
-          preview: "Louise",
-          value: "@louise"
-        }
-      ].filter(i => i.preview.toLowerCase().includes(text.toLowerCase()));
-      accept(suggestions);
-    }, 250);
-  });
-}
-
-const converter = new Showdown.Converter({
-  tables: true,
-  simplifiedAutoLink: true,
-  strikethrough: true,
-  tasklists: true
-});
 
 const NewPostPage = () => {
   const tagOptions = [
-    { label: 'London', value: 'london' },
-    { label: 'Sydney', value: 'sydney' },
-    { label: 'Shanghai', value: 'shanghai' },
-  ]
+    { label: "London", value: "london" },
+    { label: "Sydney", value: "sydney" },
+    { label: "Shanghai", value: "shanghai" }
+  ];
+  const [title, setTitle] = React.useState();
+  const [coverImage, setCoverImage] = React.useState();
+  const [slug, setSlug] = React.useState();
+  const [imageIsLoading, setImageIsLoading] = React.useState(false);
+  const [tags, setTags] = React.useState([]);
   const [value, setValue] = React.useState("**Hello world!!!**");
   const [selectedTab, setSelectedTab] = React.useState("write");
+  const [toasts, setToast] = useToasts();
+  const router = useRouter();
+
+  // const { visible, setVisible, bindings } = useModal()
+
+  function inputValidate() {
+    if (typeof (title) == "undefined" || !title || title.length === 0) {
+      setToast({
+        type: "warning",
+        text: "Title cannot bt empty"
+      });
+      return false;
+    }
+    if (typeof (slug) == "undefined" || !slug || slug.length === 0) {
+      setToast({
+        type: "warning",
+        text: "Slug cannot be empty"
+      });
+      return false;
+    }
+    return true;
+  }
+
+  const handleSubmit = async (published, e) => {
+    if (!inputValidate()) {
+      return;
+    }
+    const url = "/api/posts";
+    const data = {
+      title: title,
+      coverImage: coverImage,
+      slug: slug,
+      rawContent: value,
+      published: published
+    };
+    await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(res => {
+      if (res.ok && res.status === 201) {
+        setToast({
+          type: "success",
+          text: "Post sent successfully"
+        });
+        router.push("/dashboard");
+      } else {
+        setToast({
+          type: "warning",
+          text: "Failed to send comment."
+        });
+        console.log("failed to send comment: ", res.status);
+      }
+    }).catch(error => {
+      console.log("error: ", error.message);
+    });
+  };
+
+  function handleImageIconClick() {
+    setImageIsLoading(true);
+    fetch("https://source.unsplash.com/random/2100x900")
+      .then((res) => {
+        setCoverImage(res.url);
+        setImageIsLoading(false);
+      });
+  }
+
+  const save = async function* (data) {
+    // Promise that waits for "time" milliseconds
+    const wait = function(time) {
+      return new Promise((a, r) => {
+        setTimeout(() => a(), time);
+      });
+    };
+
+    // Upload "data" to your server
+    // Use XMLHttpRequest.send to send a FormData object containing
+    // "data"
+    // Check this question: https://stackoverflow.com/questions/18055422/how-to-receive-php-image-data-over-copy-n-paste-javascript-with-xmlhttprequest
+
+    await wait(2000);
+    // yields the URL that should be inserted in the markdown
+    yield "https://picsum.photos/300";
+    await wait(2000);
+
+    // returns true meaning that the save was successful
+    return true;
+  };
+
   return (
     <>
       {/*<Header />*/}
       <Page.Body>
-        <Grid.Container justify={"center"} style={{ marginTop: '-35px', paddingLeft: '3%', paddingRight: '3%' }}>
-          <Grid xs={24} md={14}>
+        <Grid.Container justify={"center"} style={{ marginTop: "-35px", paddingLeft: "3%", paddingRight: "3%" }}>
+          <Grid xs={24} md={18} lg={16} xl={14}>
             <Col>
               <Row>
-                <Input icon={<Icon.Type />} placeholder={"Title"} width="100%" size={"large"}>
+                <Input icon={<Icon.Type />} placeholder={"Title"} width="100%" size={"large"}
+                       value={title} onChange={e => {
+                  setTitle(e.target.value);
+                  setSlug(e.target.value.replace(/\s+/g, "-").toLowerCase());
+                }}>
                   <Text h2>Write a new post</Text>
+                </Input>
+              </Row>
+              <Row>
+                <Spacer y={0.5} />
+              </Row>
+              <Row>
+                <Input icon={imageIsLoading ? <Loading /> : <Icon.Image />} placeholder={"Cover Image URL"} width="100%"
+                       size={"large"}
+                       value={coverImage} onChange={e => setCoverImage(e.target.value)}
+                       iconClickable onIconClick={handleImageIconClick}>
+                  {/*TODO: add loading*/}
                 </Input>
               </Row>
               <Row>
@@ -66,11 +159,13 @@ const NewPostPage = () => {
               <Row>
                 <Grid.Container gap={0.5}>
                   <Grid xs={24} lg={12}>
-                    <Input width="100%" icon={<Icon.Paperclip />} placeholder={"Your slug just like hello-world"}>
+                    <Input width="100%" icon={<Icon.Paperclip />} placeholder={"Your Slug (e.g. hello-world)"}
+                           value={slug} onChange={e => setSlug(e.target.value)}>
                     </Input>
                   </Grid>
                   <Grid xs={24} lg={12}>
-                    <AutoComplete icon={<Icon.Hash />} width="100%" placeholder={"Add up to 4 tags..."} options={tagOptions}/>
+                    <AutoComplete icon={<Icon.Hash />} width="100%" placeholder={"Add Up To 4 Tags..."}
+                                  options={tagOptions} />
                   </Grid>
                 </Grid.Container>
               </Row>
@@ -78,31 +173,26 @@ const NewPostPage = () => {
                 <Spacer y={0.5} />
               </Row>
               <Row>
-                <Card style={{ width: '100%', height: '65vh' }}>
-                  <div className="container" style={{ height: 'auto' }}>
-                    <Editor
-                      maxLength={100000}
-                      scrollTo={true}
-                      placeholder={"Write your post here..."}
+                <Card style={{ width: "100%", height: "60vh" }}>
+                  <div className="container" style={{ width: "108%", margin: "-22px" }}>
+                    <ReactMde
+                      value={value}
+                      onChange={setValue}
+                      selectedTab={selectedTab}
+                      onTabChange={setSelectedTab}
+                      generateMarkdownPreview={(markdown) =>
+                        Promise.resolve(<ReactMarkdown source={markdown} />)
+                      }
+                      childProps={{
+                        writeButton: {
+                          tabIndex: -1
+                        }
+                      }}
+                      paste={{
+                        saveImage: save
+                      }}
                     />
                   </div>
-                  {/*<div className="container">*/}
-                  {/*  <ReactMde*/}
-                  {/*    value={value}*/}
-                  {/*    onChange={setValue}*/}
-                  {/*    selectedTab={selectedTab}*/}
-                  {/*    onTabChange={setSelectedTab}*/}
-                  {/*    generateMarkdownPreview={markdown =>*/}
-                  {/*      Promise.resolve(converter.makeHtml(markdown))*/}
-                  {/*    }*/}
-                  {/*    loadSuggestions={loadSuggestions}*/}
-                  {/*    childProps={{*/}
-                  {/*      writeButton: {*/}
-                  {/*        tabIndex: -1*/}
-                  {/*      }*/}
-                  {/*    }}*/}
-                  {/*  />*/}
-                  {/*</div>*/}
                 </Card>
               </Row>
               <Row>
@@ -111,10 +201,12 @@ const NewPostPage = () => {
               <Row>
                 <Grid.Container gap={0.5}>
                   <Grid xs={24} sm={12} md={6} lg={4}>
-                    <Button auto type="success-light" style={{ width: '100%' }}><Text b>publish</Text></Button>
+                    <Button auto type="success-light" style={{ width: "100%" }}
+                            onClick={(e) => handleSubmit(true, e)}><Text b>publish</Text></Button>
                   </Grid>
                   <Grid xs={24} sm={12} md={6} lg={4}>
-                    <Button auto style={{ width: '100%' }}><Text b>Save Draft</Text></Button>
+                    <Button auto style={{ width: "100%" }}
+                            onClick={(e) => handleSubmit(false, e)}><Text b>Save Draft</Text></Button>
                   </Grid>
                 </Grid.Container>
               </Row>
@@ -122,9 +214,6 @@ const NewPostPage = () => {
           </Grid>
         </Grid.Container>
       </Page.Body>
-      <Page.Footer>
-
-      </Page.Footer>
     </>
   )
 }
