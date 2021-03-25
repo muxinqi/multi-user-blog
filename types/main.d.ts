5c23d3a94cb23f8923bfeb00fed3544961428d3f
muxinqi <hi@mxq.im>
2021-03-25 16:37:16 +0800

    feat: add types definition

---
+++ types/main.d.ts
@@ -0,0 +1,41 @@
+﻿export interface User {
+  id: number;
+  name?: string | null;
+  username?: string | null;
+  email?: string | null;
+  image?: string | null;
+  createdAt: string;
+  posts?: (Post)[] | null;
+  comments?: (Comment)[] | null;
+}
+
+export interface Post {
+  id: number;
+  title: string;
+  slug: string;
+  coverImage: string;
+  createdAt: string;
+  rawContent: string;
+  renderedContent: string;
+  published: boolean;
+  author: User;
+  likesCount: number;
+  viewsCount: number;
+  tags?: (Tag)[] | null;
+  comments?: (Comment)[] | null;
+}
+
+export interface Comment {
+  id: number;
+  createdAt: string;
+  rawContent: string;
+  renderedContent: string;
+  author: User;
+  post: Post;
+}
+
+export interface Tag {
+  id: number;
+  name: string;
+  posts?: (Post)[] | null;
+}
